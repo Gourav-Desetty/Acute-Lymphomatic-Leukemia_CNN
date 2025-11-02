967e26156995217cc0c232b62135284be8aa9599
Gourav-Desetty <139993532+Gourav-Desetty@users.noreply.github.com>
2025-11-03 00:26:27 +0530

    added model_path

--- leukemia/constant/training_pipeline/__init__.py
+++ leukemia/constant/training_pipeline/__init__.py
@@ -9,6 +9,8 @@ TRAINING_DATA_DIR  = "NMC_training_data"
 TEST_DATA_PRELIM_DIR  = "test_prelim"
 TEST_DATA_FINAL_DIR  = "test_final"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+IMAGE_PATH =  random.choice(list(Path("PKG_C_NMC/test_final").glob("*/*.bmp")))
+MODEL_PATH = "../Models/leukemia_model_efficientnet_b0_01.pth"
 
 """
 DATA INGESTION REALTED CONSTANTS
@@ -16,5 +18,3 @@ DATA INGESTION REALTED CONSTANTS
 
 DATA_INGESTION_DATASET_NAME = "PKG_C_NMC"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
-
-IMAGE_PATH =  random.choice(list(Path("PKG_C_NMC/test_final").glob("*/*.bmp")))
